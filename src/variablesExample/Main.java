1efe609682510613258e03bf19c0e54a0078b8a5
mertcqn14 <mertcanciftcioglu704@gmail.com>
2021-09-28 17:02:42 +0300

    Initial commit

---
+++ src/variablesExample/Main.java
@@ -0,0 +1,30 @@
+package variablesExample;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Phone Features");
+		String phoneName=("Samsung Galaxy A21s") ;
+		System.out.println("Screen Size : 6.5 inches"); 
+		System.out.println("Internal Storage : 64gb  ");
+		System.out.println("Memory (RAM): 4gb ");
+		System.out.println("Battery Capacity: 5000mAh ");
+		System.out.println("Number of Lines: Double Line ");
+		System.out.println("Quick Charge: Yes ");
+		System.out.println("5G : No ");
+		System.out.println("4.5G Support: Yes ");
+		System.out.println("Camera Resolution :48MP");
+		System.out.println("Memory Card Max. Capacity : 512gb ");
+		System.out.println("Quick Charge Power (Max.): 15 ");
+		System.out.println("Fingerprint Reader: Yes ");
+		System.out.println("Water Resistance : No ");
+
+
+		
+	
+		
+
+	}
+
+}
